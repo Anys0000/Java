3693585e595547f10f0e7329f749eac9ddb833fd
Any <ani.sahakyan.00@gmail.com>
2019-12-12 23:02:32 +0400

    first

---
+++ untitled1/src/array.java
@@ -0,0 +1,10 @@
+public class array {
+    public static void main(String[] args){
+          int[] array = {51,136,387,56,1,2,3,6,4,95};
+
+        for (int i:array) {
+            System.out.println(i);
+        }
+    }
+
+}
