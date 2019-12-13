3ff5cee05717a5b41bdaf4c533bacf081f059f46
Any <ani.sahakyan.00@gmail.com>
2019-12-13 20:23:42 +0400

    fifth

---
+++ fifth/src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args)
+    {
+        short[] arr = new short[150];
+        for(short i = 1;i<=300;i++){
+            if(i%2!=0)
+            {
+                arr[i]=i;
+            }
+        }
+    }
+}
+
